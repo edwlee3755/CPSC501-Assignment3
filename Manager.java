068d3076798271be07549fbd680a46df03b6fe2b
edwlee <edwlee@ucalgary.ca>
2016-11-15 23:46:10 -0700

    Refactor: Move method

--- Manager.java
+++ Manager.java
@@ -13,5 +13,15 @@ public class Manager {
 			
 		}
 	}
+	
+	private static void printObjectCreationMenu()
+	{
+		System.out.println("Enter any of the following to create specified object");
+		System.out.println("(1) A simple object with only primitives for instance variables.");
+		System.out.println("(2) An object that contains references to other objects");
+		System.out.println("(3) An object that contains an array of primitives");
+		System.out.println("(4) An object that contains an array of object references");
+		System.out.println("(5) An object that uses an instance of one of Java's Collection classes to refer to several other objects");
+	}
 
 }
--- ObjectCreator.java
+++ ObjectCreator.java
@@ -6,15 +6,7 @@ public class ObjectCreator {
 		boolean go = true;
 		
 //		while (go){
-		private static void printObjectCreationMenu()
-		{
-			System.out.println("Enter any of the following to create specified object");
-			System.out.println("(1) A simple object with only primitives for instance variables.");
-			System.out.println("(2) An object that contains references to other objects");
-			System.out.println("(3) An object that contains an array of primitives");
-			System.out.println("(4) An object that contains an array of object references");
-			System.out.println("(5) An object that uses an instance of one of Java's Collection classes to refer to several other objects");
-		}
+
 			Scanner in = new Scanner(System.in);
 			int userInput = in.nextInt();
 			
